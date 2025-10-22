3fc8f362893bf9aef659bd44e92ddb5a7a687d2a
TheoD20 <theonegrao18@gmail.com>
2025-10-22 14:11:35 +0000

    Deploying to gh-pages from @ TheoD20/CAB302_Project_CI@64438b8d3af50377f9d508343005115f867f878f 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.cab302_project","l":"com.app.studysnap"},{"m":"com.example.cab302_project","l":"com.app.studysnap.controllers"},{"m":"com.example.cab302_project","l":"com.app.studysnap.model"},{"m":"com.example.cab302_project","l":"com.app.studysnap.services"}];updateSearchResults();
